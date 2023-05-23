1e62e2ca1adfca251934bbf12bd8f01616120e3a
cleberson <clebersonweng@gmail.com>
2023-05-22 20:29:29 -0400

    change the quantity images 20 to 400

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ const getPhotos = async () => {
 
 
 export const getServerSideProps = async () => {
-   const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image?max_results=20`, {
+   const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image?max_results=400`, {
       method: "GET",
       headers: {
          Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')} }`
